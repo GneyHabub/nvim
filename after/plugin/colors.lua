361810027fc26f6a39a91cd108c4969affc0cea8
AlexanderKrivonosov <mr.afigitelniychuvak@gmail.com>
2023-08-25 21:39:18 +0200

    Commit

---
+++ after/plugin/colors.lua
@@ -0,0 +1,7 @@
+
+function ColorMyPencils(color)
+	color = color or "nordic"
+        vim.cmd.colorscheme(color)
+end
+
+ColorMyPencils()
---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,10 @@
+require("nvim-tree").setup({
+git = {
+  enable = true,
+  ignore = false,
+  timeout = 500,
+},
+})
+
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<C-f>", ":NvimTreeFocus<CR>")
