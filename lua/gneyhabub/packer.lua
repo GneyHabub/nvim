156fe5878544f0d6bc70f84980ad5d5606211d5f
AlexanderKrivonosov <mr.afigitelniychuvak@gmail.com>
2023-08-25 21:41:25 +0200

    Fix name

---
+++ lua/gneyhabub/packer.lua
@@ -0,0 +1,69 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use ('theprimeagen/harpoon')
+  use ('mbbill/undotree')
+  use ('tpope/vim-fugitive')
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+use ('github/copilot.vim')
+use ('ThePrimeagen/vim-be-good')
+use ({'folke/tokyonight.nvim',
+	  as = 'tokyonight',
+	  config= function()
+		  vim.cmd('colorscheme tokyonight')
+	  end
+  })
+use ({
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+})
+use ('nvim-tree/nvim-web-devicons')
+use({
+  "dnlhc/glance.nvim",
+  config = function()
+    require('glance').setup({
+      -- your configuration
+    })
+  end,
+})
+use ({
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional
+  },
+})
+use ('airblade/vim-gitgutter')
+use { 'embark-theme/vim', as = 'embark' }
+use {'AlexvZyl/nordic.nvim', as = 'nordic'}
+use ({'folke/todo-comments.nvim', requires = 'nvim-lua/plenary.nvim'})
+end)
