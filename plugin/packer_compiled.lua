24dd553f9468d4d5059332efa818fb85f49b3510
Alexander Krivonosov <31561808+GneyHabub@users.noreply.github.com>
2024-06-14 12:57:54 +0200

    Add nice cmdline

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -100,6 +100,11 @@ _G.packer_plugins = {
     path = "/Users/alexanderkrivonosov/.local/share/nvim/site/pack/packer/start/distant.nvim",
     url = "https://github.com/chipsenkbeil/distant.nvim"
   },
+  ["fine-cmdline.nvim"] = {
+    loaded = true,
+    path = "/Users/alexanderkrivonosov/.local/share/nvim/site/pack/packer/start/fine-cmdline.nvim",
+    url = "https://github.com/VonHeikemen/fine-cmdline.nvim"
+  },
   ["glance.nvim"] = {
     config = { "\27LJ\2\n8\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\vglance\frequire\0" },
     loaded = true,
